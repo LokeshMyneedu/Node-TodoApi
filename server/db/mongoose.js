e0d5a37f751146afaf6591d7f8104b46349a3099
Lokesh Myneedu <nmyneedu@avidxchange.com>
2018-03-03 16:06:47 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://nmyneedu:Khajaguda@6@ds155288.mlab.com:55288/todo-app-api2');
 
 module.exports ={
     mongoose

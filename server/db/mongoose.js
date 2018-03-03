82859e970b98f285934d4b5f40fb2c85b084a911
Lokesh Myneedu <nmyneedu@avidxchange.com>
2018-03-03 16:40:01 -0500

    changed mongodb connection instane

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<nmyneedu>:<Khajaguda@6>@ds155288.mlab.com:55288/todo-app-api2');
-
-module.exports ={
-    mongoose
-};
+const REMOTE_MONGO = 'mongodb://nmyneedu:Khajaguda_6@ds155288.mlab.com:55288/todo-app-api2';
+                const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+                const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+                
+                mongoose.Promise = global.Promise;
+                mongoose.connect(MONGO_URI).then(() => {
+                    console.log('Connected to Mongo instance.')
+                }, (err) => {
+                    console.log('Error connecting to Mongo instance: ', err);
+                });
+                
+                module.export = { mongoose };

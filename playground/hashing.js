68396c2b9c541b70efb90db88f3c83321c9cdf94
Lokesh Myneedu <nmyneedu@avidxchange.com>
2018-03-05 10:31:55 -0500

    password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,22 @@
 const {SHA256} = require('crypto-js');
 
+const bcrypt = require('bcryptjs');
+
 let message = 'I am user number 3';
 
 let hash = SHA256('message').toString();
+
+let password = '123abc!';
+
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(hash)=>{
+        console.log(hash);
+    })
+});
+
+bcrypt.compare('123abc!','ab530a13e45914982b79f9b7e3fba994cfd1f3fb22f71cea1afbf02b460c6d1d',(err,res)=>{
+    console.log(res);
+})
 
 console.log(`message :${message}`);
 
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
     email:{
@@ -72,6 +73,20 @@ userSchema.methods.generateAuthToken = function(){
         return token;
     })
 }
+
+userSchema.pre('save',function(next){
+let user = this;
+if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(user.password,salt,(err,hash)=>{
+            user.password=hash;
+            next();
+        })
+    });
+}else{
+    next();
+}
+});
 var User = mongoose.model('User',userSchema);
 
 module.exports ={User};
